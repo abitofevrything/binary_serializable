e9b41297b0136a0e8eed343356aeaeb5ec540519
Mylo Fawcett <mylo.fawcett@gmail.com>
2024-06-21 10:40:54 +0200

    binary_serializable 0.2.0

--- packages/binary_serializable/lib/binary_serializable.dart
+++ packages/binary_serializable/lib/binary_serializable.dart
@@ -1,6 +1,7 @@
 export 'src/binary_conversion.dart';
 export 'src/binary_serializable.dart';
 export 'src/binary_type.dart';
+export 'src/composite_binary_conversion.dart';
 
 export 'src/built_in/array.dart';
 export 'src/built_in/bool.dart';
@@ -9,5 +10,6 @@ export 'src/built_in/int.dart';
 export 'src/built_in/length_prefixed_list.dart';
 export 'src/built_in/string.dart';
 export 'src/built_in/unknown_length_list.dart';
+export 'src/built_in/buffer.dart';
 
 export 'dart:typed_data' show BytesBuilder, Uint8List;
