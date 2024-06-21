46bba0c99d5cbed8dff94f40eaaa7b28e7d06133
Mylo Fawcett <mylo.fawcett@gmail.com>
2024-06-21 10:39:59 +0200

    Add buffer type

---
+++ packages/binary_serializable/lib/src/built_in/buffer.dart
@@ -0,0 +1,44 @@
+import 'dart:typed_data';
+
+import 'package:binary_serializable/src/binary_conversion.dart';
+import 'package:binary_serializable/src/binary_type.dart';
+
+class BufferType extends BinaryType<Uint8List> {
+  final int length;
+
+  const BufferType(this.length);
+
+  @override
+  Uint8List encode(Uint8List input) => input;
+
+  @override
+  BinaryConversion<Uint8List> startConversion(
+          void Function(Uint8List p1) onValue) =>
+      BufferConversion(length, onValue);
+}
+
+class BufferConversion extends BinaryConversion<Uint8List> {
+  final int length;
+
+  final BytesBuilder _builder = BytesBuilder();
+
+  BufferConversion(this.length, super.onValue);
+
+  @override
+  int add(Uint8List data) {
+    if (_builder.length + data.length >= length) {
+      final taken = Uint8List.sublistView(data, 0, length - _builder.length);
+      _builder.add(taken);
+      onValue(_builder.takeBytes());
+      return taken.length;
+    } else {
+      _builder.add(data);
+      return data.length;
+    }
+  }
+
+  @override
+  void flush() {
+    if (_builder.isNotEmpty) throw 'flushed while converting buffer';
+  }
+}
